eef2c4a7762750c7f16fa5d21d6079691c6c1cf8
Jason <>
2023-12-10 08:55:12 +0800

    缩短间隔时间

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func main() {
 
 	//开始mint
 	for i := 1; i <= config.MintConfig.Times; i++ {
-		time.Sleep(5 * time.Second) //防止多次异常，导致连续mint同一个nonce
+		time.Sleep(3 * time.Second) //防止多次异常，导致连续mint同一个nonce
 		balanceStr, er := evmApp.TokenBalanceOf()
 		err = er
 		if err != nil {
